0463836ed5339382b41871dfaab213e24818d20c
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-25 18:20:11 +0530

    added products_list_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
-void main(){
+import 'package:flutter/material.dart';
+import 'package:products_tutorial/pages/products_list_page.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: ProductsListPage(),
+    ),
+  );
 }
---
+++ lib/pages/products_list_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductsListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
