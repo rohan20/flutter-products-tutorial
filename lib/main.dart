12b912637795b39fd95360d5b4a2bfd1cc385837
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-26 13:54:38 +0530

    implemented ProductListItem

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:products_tutorial/pages/products_list_page.dart';
+import 'package:products_tutorial/widgets/product_list_item.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: ProductsListPage(),
+      home: ProductListItem(),
     ),
   );
 }
