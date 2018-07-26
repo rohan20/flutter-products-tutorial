28038eff909e340613788318a33e25f838803259
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-26 13:16:58 +0530

    added filter widgets

--- lib/pages/products_list_page.dart
+++ lib/pages/products_list_page.dart
@@ -1,21 +1,78 @@
 import 'package:flutter/material.dart';
 
 class ProductsListPage extends StatelessWidget {
+  BuildContext context;
+
   @override
   Widget build(BuildContext context) {
+    this.context = context;
+
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.white,
-        title: Center(
-          child: Text(
-            "PRODUCT LIST",
-            style: TextStyle(
-              color: Colors.black,
-            ),
+        title: Text(
+          "PRODUCT LIST",
+          style: TextStyle(
+            color: Colors.black,
           ),
         ),
       ),
-      body: Container(),
+      body: _buildProductsListPage(),
+    );
+  }
+
+  _buildProductsListPage() {
+    Size screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: screenSize.width,
+      height: screenSize.height,
+      color: Colors.grey[200],
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(12.0),
+            padding: const EdgeInsets.all(12.0),
+            width: screenSize.width,
+            color: Colors.white,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildFilterButton("SORT"),
+                Container(
+                  color: Colors.black,
+                  width: 2.0,
+                  height: 24.0,
+                ),
+                _buildFilterButton("REFINE"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildFilterButton(String title) {
+    return InkWell(
+      onTap: () {
+        print(title);
+      },
+      child: Row(
+        children: <Widget>[
+          Icon(
+            Icons.arrow_drop_down,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 2.0,
+          ),
+          Text(title),
+        ],
+      ),
     );
   }
 }
