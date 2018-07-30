0e6ee02cc0d1e7d82906a751b6c14bf41ec726f0
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-30 12:24:23 +0530

    declared routes in the MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:products_tutorial/pages/product_detail_page.dart';
 import 'package:products_tutorial/pages/products_list_page.dart';
+import 'package:products_tutorial/util/routes.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: ProductsListPage(),
+      routes: Routes.routes,
     ),
   );
 }
